f55da6535266f240b3678db8f9813f26964ba36f
eli1809 <eleman1809@gmail.com>
2019-05-01 20:56:51 +0300

    added RouteController

---
+++ MatakAPI/Controllers/RouteController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MatakDBConnector;
+
+
+namespace MatakAPI.Controllers
+{
+    [ApiController]
+    public class RouteController : Controller
+    {
+        [HttpPost("GetAllRoutes")]
+        public IActionResult GetAllRoutes()
+        {
+            string errorString = null;
+            GetRoute getter = new GetRoute();
+
+            List<Route> obj = getter.AllRoutes(out errorString);
+            return new JsonResult(obj);
+
+        }
+        [HttpGet("GetRoute/{id}")]
+        public IActionResult GetRoute(int id)
+        {
+            string errorString = null;
+            GetRoute getter = new GetRoute();
+            Route myRoute = getter.ById(id, out errorString);
+            return new JsonResult(myRoute);
+        }
+
+        [HttpPost("SetRoute")]
+        public IActionResult SetRoute()
+        {
+            return Ok("You have Update the Route.");
+        }
+        [HttpPost("DeleteRoute")]
+        public IActionResult DeleteRoute()
+        {
+            return Ok("You have Deleted the Route.");
+        }
+
+        [HttpGet("HomePage")]
+        public ActionResult<IEnumerable<string>> HomePage()
+        {
+            return Ok(
+                "** Welcome to matak APP ** "
+                + System.Environment.NewLine +
+                " all the methodes are [HttpPost]"
+                + System.Environment.NewLine +
+                " https://matakcloud.azurewebsites.net/GetAllRoutes - get all the routes  "
+                 + System.Environment.NewLine +
+                " https://matakcloud.azurewebsites.net/GetRoute/{id} - get spesific route "
+            );
+        }
+        [HttpGet("")]
+        public ActionResult<IEnumerable<string>> startPage()
+        {
+            return Ok(
+                "** Welcome to matak APP ** "
+                + System.Environment.NewLine +
+                " all the methodes are [HttpPost]"
+                + System.Environment.NewLine +
+                " https://matakcloud.azurewebsites.net/GetAllRoutes - get all the routes  "
+                 + System.Environment.NewLine +
+                " https://matakcloud.azurewebsites.net/GetRoute/{id} - get spesific route "
+            );
+        }
+    }
+}
